cc920749221ae14824c7671387d73f3a52c03149
rowlandg <rg@rogueapp.com>
2022-08-29 14:23:08 +0100

    add dotenv

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import * as dotenv from "dotenv";
 import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from "@auth0/auth0-react";
 import Main from './Main';
 import "./index.css";
+
+dotenv.config();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
 );
 root.render(
   <Auth0Provider
-    // domain={process.env.REACT_APP_AUTH0_DOMAIN}
-    domain="test-pisys.eu.auth0.com"
-    clientId="H7CJZjfszFDaQfTObOaF1G7MEDmlFxNS"
-    // redirectUri={window.location.origin}
-    redirectUri="http://localhost:3000/"
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+    redirectUri={window.location.origin}
+    //redirectUri="http://localhost:3000/"
   >
     <Main />
   </Auth0Provider>
